0d6f3af46085223f918dbf86fce38e44c8b93c30
jm0sp1923 <jm0sp1923@gmail.com>
2024-12-19 10:21:14 -0500

    Cambio ruta volumen uploads

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 import fetch from 'node-fetch';
-import { writeFile } from 'fs/promises';
+import { writeFile, mkdir } from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
@@ -13,36 +13,50 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // Configura la carpeta donde estarán tus plantillas
-const port = process.env.PORT || 3000 ;
+app.set('views', path.join(__dirname, 'views'));
+const port = process.env.PORT || 3000;
 
 // Middleware para manejar JSON
 app.use(express.json());
 
+// Usar la ruta del volumen montado '/uploads' en Railway
+const uploadsDir = '/uploads'; // Ruta montada en Railway
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// Middleware para servir archivos estáticos desde la ruta de uploads
+app.use('/uploads', express.static(uploadsDir));
 
 app.post('/webhook', async (req, res) => {
-  console.log("Webhook")
-  const exportId = process.env.EXPORT_ID
+  console.log("Webhook recibido");
+  const exportId = process.env.EXPORT_ID;
   const { id: dataId, data: { form_id: formId } } = req.body;
   console.log('Webhook recibido:', req.body);
+
   try {
     const response = await fetch(`https://forms.kizeo.com/rest/v3/forms/${formId}/data/${dataId}/exports/${exportId}/pdf`, {
       method: 'GET',
       headers: {
         Authorization: process.env.KIZEO_API_KEY
       }
-    }); 
+    });
 
     if (!response.ok) {
       throw new Error(`Error al obtener el PDF: ${response.statusText}`);
     }
+
     const fileName = response.headers.get('x-filename-custom') || 'archivo.pdf';
     const blob = await response.blob();
     const buffer = Buffer.from(await blob.arrayBuffer());
 
-    const filePath = path.join(__dirname, 'uploads', fileName);
+    // Crear el directorio '/uploads' si no existe
+    try {
+      await mkdir(uploadsDir, { recursive: true });
+      console.log(`Directorio 'uploads' creado o ya existe`);
+    } catch (err) {
+      console.error('Error al crear directorio de uploads:', err);
+      return res.status(500).json({ error: 'Error al crear directorio de uploads' });
+    }
+
+    const filePath = path.join(uploadsDir, fileName);
     await writeFile(filePath, buffer);
 
     console.log(`PDF descargado exitosamente como ${fileName}`);
@@ -54,13 +68,9 @@ app.post('/webhook', async (req, res) => {
   }
 });
 
-
-
-
 app.get('/', (req, res) => {
   res.render('index');
 });
-
 
 app.get('/index', (req, res) => {
   res.status(200).json({ message: 'index' });

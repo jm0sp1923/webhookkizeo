60d05e45df0a24739081f76377be02fc2adf402b
jm0sp1923 <jm0sp1923@gmail.com>
2024-12-20 15:17:38 -0500

    Asignacion zona automatica

--- routes/webhook.js
+++ routes/webhook.js
@@ -14,13 +14,12 @@ const site_url = process.env.SITE_URL;
 router.post('/webhook', async (req, res) => {
   const { id: dataId, data: { form_id: formId, fields: fields_id } } = req.body;
 
-  console.log('Datos recibidos:', JSON.stringify(req.body, null, 2));
-
   try {
     // Extraer la zona
     console.log('fields_id:', JSON.stringify(fields_id, null, 2));
     
     const zona = fields_id?.result?.value?.code; // Extrae el valor de la zona
+    
     if (!zona) {
       throw new Error('Zona no encontrada en los datos recibidos.');
     }

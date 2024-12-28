c7b597720995113fb9cec0475e589ab0e281e8e4
jm0sp1923 <jm0sp1923@gmail.com>
2024-12-28 11:07:19 -0500

    import express

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,4 +1,4 @@
-
+import express from 'express';
 import fetch from 'node-fetch';
 import { ejecutarSubidaSharePoint } from '../utils/pythonUtils.js';
 import { obtenExportId } from '../utils/kizeoUtils.js';

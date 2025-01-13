442e66ccb6ab2e87890d9cd3ca7f532b2eb6cba8
jm0sp1923 <jm0sp1923@gmail.com>
2025-01-13 16:13:14 -0500

    Fusionar excels

---
+++ fusionar_excel.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import sys
+import os
+
+# Definir la constante
+NUMERO_CUENTA = 'Numero Cuenta'
+CUENTA = 'Cuenta'
+
+def fucionar_excel(archivo_datos, archivo_busqueda):
+    # Leer el archivo de datos
+    archivo_datos = pd.read_csv(
+        archivo_datos,
+        encoding='ISO-8859-1',
+        delimiter=';',
+        on_bad_lines='skip',
+        engine='python'
+    )  # Saltar las líneas con errores
+
+    # Leer el archivo de búsqueda
+    archivo_busqueda = pd.read_excel(archivo_busqueda, engine='openpyxl')
+
+    # Limpiar los nombres de las columnas
+    archivo_datos.columns = archivo_datos.columns.str.strip()
+    archivo_busqueda.columns = archivo_busqueda.columns.str.strip()
+
+    # Asegurarse de que las columnas 'Numero Cuenta' y 'Cuenta' sean de tipo str y estén limpias
+    archivo_datos[NUMERO_CUENTA] = archivo_datos[NUMERO_CUENTA].astype(str).str.strip()
+    archivo_busqueda[CUENTA] = archivo_busqueda[CUENTA].astype(str).str.strip()
+
+    # Eliminar cualquier '.0' en los números de cuenta
+    archivo_datos[NUMERO_CUENTA] = archivo_datos[NUMERO_CUENTA].str.replace('.0', '', regex=False)
+
+    # Unir los dos archivos por las columnas correspondientes
+    archivo_unido = pd.merge(archivo_datos, archivo_busqueda, left_on=NUMERO_CUENTA, right_on=CUENTA, how='inner', validate='many_to_many')
+
+    # Seleccionar las columnas necesarias
+    archivo_unido_seleccionado = archivo_unido[['Cuenta', 'Inmobiliaria', 'Nit Inmobliaria', 'Identificacion Tercero',
+                                                'Nombres / Siglas', 'Apellidos / Razon Social', 'Tipo Amparo', 'Cobertura',
+                                                'Direccion', 'Ciudad', 'Estado', 'ETAPA DE COBRO']]
+
+    # Asegurarse de que la carpeta 'processed' exista dentro del proyecto
+    project_folder = os.path.dirname(os.path.abspath(__file__))  # Obtiene el directorio del proyecto
+    processed_folder = os.path.join(project_folder, 'processed')  # Ruta completa a la carpeta 'processed'
+
+    if not os.path.exists(processed_folder):
+        os.makedirs(processed_folder)
+        print(f"Carpeta 'processed' creada en: {processed_folder}")
+    else:
+        print(f"La carpeta 'processed' ya existe en: {processed_folder}")
+
+    # Guardar el archivo procesado en la carpeta 'processed'
+    output_path = os.path.join(processed_folder, 'excel_fusionado.xlsx')
+    archivo_unido_seleccionado.to_excel(output_path, index=False)
+
+    # Confirmar que el archivo se ha guardado correctamente
+    print(f"Archivo procesado creado en: {output_path}")
+
+if __name__ == "__main__":
+    archivo_datos = sys.argv[1]
+    archivo_busqueda = sys.argv[2]
+
+    fucionar_excel(archivo_datos, archivo_busqueda)
--- routes/fucionarExcel.js
+++ routes/fucionarExcel.js
@@ -1,14 +1,85 @@
-import express from 'express';
-
+import express from "express";
+import multer from "multer";
+import path from "path";
+import { exec } from "child_process";
+import fs from "fs";
 
 const router = express.Router();
 
-router.get('/fucionarExcel', async (req, res) => {
+// Configuración de Multer para manejar la carga de archivos
+const upload = multer({ dest: "uploads/" });
+
+router.get("/fucionarExcel", async (req, res) => {
   try {
-    res.render('viewFucionarExcel'); 
+    res.render("viewFucionarExcel");
   } catch (error) {
-    res.status(500).send('Error al cargar la página');
+    res.status(500).send("Error al cargar la página");
   }
 });
+
+router.post(
+  "/fucionarExcel",
+  upload.fields([
+    { name: "reporteAfianzado", maxCount: 1 },
+    { name: "baseCartera", maxCount: 1 },
+  ]),
+  async (req, res) => {
+    try {
+      const { reporteAfianzado, baseCartera } = req.files;
+      if (!reporteAfianzado || !baseCartera) {
+        return res.status(400).json({
+          success: false,
+          message: "Ambos archivos son requeridos.",
+        });
+      }
+
+      const archivoDatos = reporteAfianzado[0].path;
+      const archivoBusqueda = baseCartera[0].path;
+
+      const scriptPath = path.resolve("fusionar_excel.py");
+      const outputFilePath = path.resolve(
+        "/processed/excel_fusionado.xlsx"
+      );
+
+      const command = `python "${scriptPath}" "${archivoBusqueda}" "${archivoDatos}" "${outputFilePath}"`;
+
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error ejecutando el script Python: ${error.message}`);
+          console.error(`Comando ejecutado: ${command}`);
+          return res.status(500).json({
+            success: false,
+            message: "Error al procesar los archivos.",
+          });
+        }
+
+        if (stderr) {
+          console.error(`Error del script Python: ${stderr}`);
+        }
+
+        console.log(`Resultado del script: ${stdout}`);
+
+        res.json({
+          success: true,
+          message: "Archivos procesados exitosamente.",
+          downloadUrl: `/processed/excel_fusionado.xlsx`,
+        });
+        
+
+        // Limpieza de archivos temporales
+        fs.unlinkSync(archivoDatos);
+        fs.unlinkSync(archivoBusqueda);
+      });
+    } catch (error) {
+      console.error("Error al procesar los archivos:", error);
+      res
+        .status(500)
+        .json({ success: false, message: "Error al procesar los archivos." });
+    }
+  }
+);
+
+// Ruta para servir los archivos procesados
+router.use("/processed", express.static(path.resolve("processed")));
 
 export default router;
